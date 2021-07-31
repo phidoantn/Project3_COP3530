939d9df873825c586bf39306d83f3caa195f620b
olivia-813 <73865366+olivia-813@users.noreply.github.com>
2021-07-31 15:46:30 -0400

    update buttons

--- Screen.h
+++ Screen.h
@@ -1,11 +1,13 @@
+
 #include <SFML/Graphics.hpp>
 
 class Screen {
 public:
     Screen();
-    void Draw(sf::RenderWindow& window);
-    void LeftClick(sf::Vector2i mousePosition);
-    void RightClick(sf::Vector2i mousePosition);
+    void Draw(sf::RenderWindow& window, sf::Vector2i mousePosition);
+    void TopButton(sf::RenderWindow &window);
+    void BottomButton(sf::RenderWindow &window);
 };
+
 
 
