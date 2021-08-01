8e5dc9dd9cb40570a8d8e6359475dfda317b1227
Phi Doan <67439394+phidoantn@users.noreply.github.com>
2021-08-01 11:00:46 -0500

    Insert BST tree works

--- Screen.cpp
+++ Screen.cpp
@@ -10,39 +10,44 @@ Screen::Screen() {
 
 }
 void Screen::insertBST() {
+ 
     ifstream file;
-    file.open("halfsize.csv");
+    file.open("dna.csv");
 
-    if(!file.is_open()){
+    if (!file.is_open()) {
         cout << "dna file failure" << endl;
     }
 
     string line, rsid, ch, pos, result;
     int iter = 0;
 
-    while(getline(file, line)){                                     //gets line
+    //gets line, reach each line
+    while (getline(file, line) && iter < 30) {                                     
 
         istringstream stream(line);
-        cout << "entered while loop" << endl;
-        cout << "line: " << iter << endl;
-        if(iter > 13) {                                                      //dont want first 13 lines
+       
+        //obmit first 13 lines 
+        if (iter > 12) {                                                      
             getline(stream, rsid, ',');
-            getline(stream, ch, ',') ;
-            getline(stream, pos, ',') ;
-            getline(stream, result, '"') ;
+            getline(stream, ch, ',');
+            getline(stream, pos, ',');
+            getline(stream, result, '"');
+            
+            //don't need the rs, just want the integer part
+            string withoutRS = rsid.substr(3, rsid.length());           
 
-            string withoutRS = rsid.substr(2, rsid.length());           //don't need the rs, just want the integer part
-
-            cout << "rsid " << rsid << ". result " << result << endl;
-            tree.insertNode(tree.root, stoi(withoutRS), result);
+            // push into the tree
+            tree.root = tree.insertNode(tree.root, stoi(withoutRS), result);
         }
         iter++;
     }
+
+    file.close();
 }
 
-void Screen::Draw(sf::RenderWindow &window, sf::Vector2i mousePosition) {
+void Screen::Draw(sf::RenderWindow& window, sf::Vector2i mousePosition) {
     sf::Font font;
-    if(!font.loadFromFile("font.otf")){
+    if (!font.loadFromFile("font.otf")) {
         cout << "error loading font" << endl;
     }
     /// buttons ///
@@ -114,153 +119,154 @@ void Screen::Draw(sf::RenderWindow &window, sf::Vector2i mousePosition) {
     int widthRB = 415;
 
     //if top button clicked
-    if(mousePosition.y  > heightB && mousePosition.y < heightT && mousePosition.x > widthL && mousePosition.x < widthR){
+    if (mousePosition.y > heightB && mousePosition.y < heightT && mousePosition.x > widthL && mousePosition.x < widthR) {
         TopButton(window);
     }
     //if bottom button clicked
-    if(mousePosition.y  > heightBB && mousePosition.y < heightTB && mousePosition.x > widthLB && mousePosition.x < widthRB){
+    if (mousePosition.y > heightBB && mousePosition.y < heightTB && mousePosition.x > widthLB && mousePosition.x < widthRB) {
         BottomButton(window);
     }
 
 
 }
 
-void Screen::TopButton(sf::RenderWindow &window) {
+void Screen::TopButton(sf::RenderWindow& window) {
     sf::Font font;
-    if(!font.loadFromFile("font.otf")){
+    if (!font.loadFromFile("font.otf")) {
         cout << "error loading font" << endl;
     }
-        /// subtitles ///
-        sf::Text s1;
-        s1.setFont(font);
-        s1.setString("Build DNA");
-        s1.setCharacterSize(45);
-        s1.setFillColor(sf::Color::Black);
-        s1.setPosition(680.f, 40.f);
-        s1.setStyle(sf::Text::Bold);
-        s1.setStyle(sf::Text::Underlined);
+    /// subtitles ///
+    sf::Text s1;
+    s1.setFont(font);
+    s1.setString("Build DNA");
+    s1.setCharacterSize(45);
+    s1.setFillColor(sf::Color::Black);
+    s1.setPosition(680.f, 40.f);
+    s1.setStyle(sf::Text::Bold);
+    s1.setStyle(sf::Text::Underlined);
 
-        window.draw(s1);
+    window.draw(s1);
 
-        /// sub-subtitles ///
-        sf::Text ss1;
-        ss1.setFont(font);
-        ss1.setString("Traits:");
-        ss1.setCharacterSize(35);
-        ss1.setFillColor(sf::Color::Black);
-        ss1.setPosition(850.f, 140.f);
-        ss1.setStyle(sf::Text::Bold);
+    /// sub-subtitles ///
+    sf::Text ss1;
+    ss1.setFont(font);
+    ss1.setString("Traits:");
+    ss1.setCharacterSize(35);
+    ss1.setFillColor(sf::Color::Black);
+    ss1.setPosition(850.f, 140.f);
+    ss1.setStyle(sf::Text::Bold);
 
-        window.draw(ss1);
+    window.draw(ss1);
 
-        /// dna helix ///
-        sf::Texture tex;
-        if(!tex.loadFromFile("dnahelix.png")){
-            cout << "helix did not load correctly" << endl;
-        }
-        sf::Sprite helix;
-        helix.setTexture(tex);
-        helix.setScale(sf::Vector2f(2.2f, 2.2f));
-        helix.setPosition(580.f, 130.f);
-
-        window.draw(helix);
-
+    /// dna helix ///
+    sf::Texture tex;
+    if (!tex.loadFromFile("files/dnahelix.png")) {
+        cout << "helix did not load correctly" << endl;
     }
+    sf::Sprite helix;
+    helix.setTexture(tex);
+    helix.setScale(sf::Vector2f(2.2f, 2.2f));
+    helix.setPosition(580.f, 130.f);
+
+    window.draw(helix);
+
+}
 
 
-void Screen::BottomButton(sf::RenderWindow &window) {
+void Screen::BottomButton(sf::RenderWindow& window) {
     sf::Font font;
-    if(!font.loadFromFile("font.otf")){
+    if (!font.loadFromFile("font.otf")) {
         cout << "error loading font" << endl;
     }
-        /// subtitles ///
-        sf::Text s2;
-        s2.setFont(font);
-        s2.setString("Build Traits");
-        s2.setCharacterSize(45);
-        s2.setFillColor(sf::Color::Black);
-        s2.setPosition(760.f, 40.f);
-        s2.setStyle(sf::Text::Bold);
-        s2.setStyle(sf::Text::Underlined);
+    /// subtitles ///
+    sf::Text s2;
+    s2.setFont(font);
+    s2.setString("Build Traits");
+    s2.setCharacterSize(45);
+    s2.setFillColor(sf::Color::Black);
+    s2.setPosition(760.f, 40.f);
+    s2.setStyle(sf::Text::Bold);
+    s2.setStyle(sf::Text::Underlined);
 
-        window.draw(s2);
+    window.draw(s2);
 
-        /// sub-subtitles ///
-        sf::Text ss2;
-        ss2.setFont(font);
-        ss2.setString("DNA Sequence:");
-        ss2.setCharacterSize(35);
-        ss2.setFillColor(sf::Color::Black);
-        ss2.setPosition(850.f, 140.f);
-        ss2.setStyle(sf::Text::Bold);
+    /// sub-subtitles ///
+    sf::Text ss2;
+    ss2.setFont(font);
+    ss2.setString("DNA Sequence:");
+    ss2.setCharacterSize(35);
+    ss2.setFillColor(sf::Color::Black);
+    ss2.setPosition(850.f, 140.f);
+    ss2.setStyle(sf::Text::Bold);
 
-        window.draw(ss2);
+    window.draw(ss2);
 
 
-        /// man/woman ///
-        sf::Texture tex2;
-        if(!tex2.loadFromFile("body.png")){
-            cout << "head did not load correctly" << endl;
-        }
-        sf::Sprite body;
-        body.setTexture(tex2);
-        body.setPosition(500.f, 1.f);
-        body.setScale(sf::Vector2f(2.0f, 2.0f));
+    /// man/woman ///
+    sf::Texture tex2;
+    if (!tex2.loadFromFile("files/body.png")) {
+        cout << "head did not load correctly" << endl;
+    }
+    sf::Sprite body;
+    body.setTexture(tex2);
+    body.setPosition(500.f, 1.f);
+    body.setScale(sf::Vector2f(2.0f, 2.0f));
 
-        window.draw(body);
+    window.draw(body);
 
-        ///build tree
-        insertBST();
-        ///get vector of traits
-        tree.searchBST(tree.root);
-        ///gender
-        if(tree.traits.at(0) == "GG"){  //"boy"
-            sf::Texture s;
-            if(!s.loadFromFile("images/blueshirt.jpg")){
-                cout << "blue shirt did not load correctly" << endl;
-            }
-            sf::Sprite blueshirt;
-            blueshirt.setTexture(s);
-            blueshirt.setScale(sf::Vector2f(1.0f, 1.0f));
-            blueshirt.setPosition(580.f, 130.f);
+    ///build tree
+    insertBST();
+    ///get vector of traits
 
-            window.draw(blueshirt);
+    //tree.searchBST(tree.root);
+    ///gender
+    if (tree.traits.at(0) == "GG") {  //"boy"
+        sf::Texture s;
+        if (!s.loadFromFile("images/blueshirt.jpg")) {
+            cout << "blue shirt did not load correctly" << endl;
         }
-        else{
-            //outfit
-        }
-        ///eyes
-        if(tree.traits.at(1) == "CT"){
-            //brown eyes
-        }
-        else if(tree.traits.at(1) == "CC"){
-            //blue eyes
-        }
-        else if(tree.traits.at(1) == "TT"){
-            //black eyes
-        }
-        else{
-            //green eyes
-        }
-        ///hair
-        if(tree.traits.at(2) == "GG"){
-            //brown hair
-        }
-        else if(tree.traits.at(2) == "GT"){
-            //blonde hair
-        }
-        else if(tree.traits.at(2) == "TT"){
-            //black hair
-        }
-        else{
-            //red hair
-        }
-        ///personality
-        if(tree.traits.at(3) == "AG"){
-            //happy
-        }
-        else{
-            //sad
-        }
+        sf::Sprite blueshirt;
+        blueshirt.setTexture(s);
+        blueshirt.setScale(sf::Vector2f(1.0f, 1.0f));
+        blueshirt.setPosition(580.f, 130.f);
+
+        window.draw(blueshirt);
+    }
+    else {
+        //outfit
+    }
+    ///eyes
+    if (tree.traits.at(1) == "CT") {
+        //brown eyes
+    }
+    else if (tree.traits.at(1) == "CC") {
+        //blue eyes
+    }
+    else if (tree.traits.at(1) == "TT") {
+        //black eyes
+    }
+    else {
+        //green eyes
+    }
+    ///hair
+    if (tree.traits.at(2) == "GG") {
+        //brown hair
+    }
+    else if (tree.traits.at(2) == "GT") {
+        //blonde hair
+    }
+    else if (tree.traits.at(2) == "TT") {
+        //black hair
+    }
+    else {
+        //red hair
+    }
+    ///personality
+    if (tree.traits.at(3) == "AG") {
+        //happy
+    }
+    else {
+        //sad
+    }
 
 }
