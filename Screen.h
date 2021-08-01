62db34b59f8f1851f74314f017fafb161372bad0
olivia-813 <73865366+olivia-813@users.noreply.github.com>
2021-08-01 10:00:58 -0400

    Update Screen.h

--- Screen.h
+++ Screen.h
@@ -1,12 +1,16 @@
+#pragma
 #include <SFML/Graphics.hpp>
 #include "BST.h"
-
 class Screen {
 public:
+    BST tree;
     Screen();
+    void insertBST();
     void Draw(sf::RenderWindow& window, sf::Vector2i mousePosition);
-    void TopButton(sf::RenderWindow& window);
-    void BottomButton(sf::RenderWindow& window);
-    
+    void TopButton(sf::RenderWindow &window);
+    void BottomButton(sf::RenderWindow &window);
 };
+
+
+
 
