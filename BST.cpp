37a471fcfdb431898c9200d648834e6352a52629
Phi Doan <67439394+phidoantn@users.noreply.github.com>
2021-08-01 10:58:31 -0500

    Insert BST tree works

--- BST.cpp
+++ BST.cpp
@@ -1,76 +1,69 @@
-#pragma
 #include "BST.h"
 #include <iostream>
+
+
 using namespace std;
-BST::BST() {
-    this->root = nullptr;
-    Node();
+
+BST::~BST()
+{
+	delete root;
 }
 
-BST::Node::Node() {
-    this->left = nullptr;
-    this->right = nullptr;
-    this->rsid = 0;
-    this->result = "";
+Node* BST::insertNode(Node* r, int rsid, string letters) {
+
+	// base case
+	if (r == nullptr) {
+		return new Node(rsid, letters);
+	};
+
+	// if id already there
+	if (r->rsid == rsid)
+		return r;
+
+	else if (rsid < r->rsid) {
+		r->left = insertNode(r->left, rsid, letters);
+	}
+	else {
+		r->right = insertNode(r->right, rsid, letters);
+	}
+	cout << "got to insertNode function " << endl;
+	return r;
 }
+/*
+void BST::searchBST(BST::Node* r) {
 
-BST::Node::Node(int rs, string res) {
-    this->rsid = rs;
-    this->result = res;
+	/// trait : rsid
+	///gender : 7620511
+	///eyes : 4959788
+	///hair : 6549120
+	///personality : 952399
+
+	vector<int> searchThese;
+	searchThese.push_back(7620511);
+	searchThese.push_back(4959788);
+	searchThese.push_back(6549120);
+	searchThese.push_back(952399);
+	int i = 0;
+	while (traits.size() < 4) {
+		if (r->rsid == searchThese.at(i)) {
+			traits.push_back(r->result);
+			continue;
+		}
+		if (r->rsid < searchThese.at(i)) {
+			r = r->right;
+			continue;
+		}
+		if (r->rsid > searchThese.at(i)) {
+			r = r->left;
+			continue;
+		}
+		i++;
+	}
 }
+*/
 
-BST::Node* BST::insertNode(BST::Node *r, int rsid, string letters) {           //root should be passed in always
-    cout << "got to insertNode function " << endl;
-    if(root == nullptr){                                                 //the tree is empty
-        Node* n = new Node(rsid, letters);
-        root = n;
-        return root;
-    }
-    if(r == nullptr){                                                   //insert here
-        Node* n = new Node(rsid, letters);
-    }
-    if(rsid < r->rsid){
-        r->left = insertNode(r->left, rsid, letters);
-    }
-    else{
-        r->right = insertNode(r->right, rsid, letters);
-    }
+Node::~Node()
+{
+	delete left;
+	delete right;
 }
-
-void BST::searchBST(BST::Node *r) {
-
-    /// trait : rsid
-    ///gender : 7620511
-    ///eyes : 4959788
-    ///hair : 6549120
-    ///personality : 952399
-
-    vector<int> searchThese;
-    searchThese.push_back(7620511);
-    searchThese.push_back(4959788);
-    searchThese.push_back(6549120);
-    searchThese.push_back(952399);
-    int i = 0;
-    while(traits.size() < 4) {
-        if(r->rsid == searchThese.at(i)){
-            traits.push_back(r->result);
-            continue;
-        }
-        if(r->rsid < searchThese.at(i)){
-            r = r->right;
-            continue;
-        }
-        if(r->rsid > searchThese.at(i)){
-            r = r->left;
-            continue;
-        }
-        i++;
-    }
-}
-
-
-
-
-
-
-
