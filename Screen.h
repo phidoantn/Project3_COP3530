d8cfec0964199879077c6143b0cc13226cfbdaf2
Phi Doan <67439394+phidoantn@users.noreply.github.com>
2021-07-31 19:17:26 -0500

    add readfile function in screen.h

--- Screen.h
+++ Screen.h
@@ -1,13 +1,12 @@
-
 #include <SFML/Graphics.hpp>
+#include "BST.h"
 
 class Screen {
 public:
     Screen();
     void Draw(sf::RenderWindow& window, sf::Vector2i mousePosition);
-    void TopButton(sf::RenderWindow &window);
-    void BottomButton(sf::RenderWindow &window);
+    void TopButton(sf::RenderWindow& window);
+    void BottomButton(sf::RenderWindow& window);
+    void readfile( BST& tree);
 };
-
-
 
