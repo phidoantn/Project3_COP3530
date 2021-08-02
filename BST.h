47e889bc41815005029f32a02ecd2d0f66093627
olivia-813 <73865366+olivia-813@users.noreply.github.com>
2021-08-02 08:37:23 -0400

    Update BST.h

--- BST.h
+++ BST.h
@@ -1,20 +1,21 @@
+
 #pragma
 #include <string>
 #include <vector>
 using namespace std;
 
 class Node {
-    public:
-        int rsid;
-        string result;
-        Node* left;
-        Node* right;
-        Node():rsid(0),result(""), left(nullptr), right(nullptr) {};
-        Node(int rs, string res) :rsid(rs), result(res), left(nullptr), right(nullptr) {};
-        ~Node();
+public:
+    int rsid;
+    string result;
+    Node* left;
+    Node* right;
+    Node():rsid(0),result(""), left(nullptr), right(nullptr) {};
+    Node(int rs, string res) :rsid(rs), result(res), left(nullptr), right(nullptr) {};
+    ~Node();
 };
 class BST {
-    
+
 public:
     Node* root;
     BST() :root(nullptr) {};
@@ -22,8 +23,6 @@ public:
     vector<string> traits;
 
     Node* insertNode(Node* r, int rsid, string result);
-    void searchBST(Node* r);
+    void searchBST(Node* r, vector<int> rsid);
 
 };
-
-
