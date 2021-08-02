0e6ee544d8c3b19b444c355542d688dc6cf6af29
olivia-813 <73865366+olivia-813@users.noreply.github.com>
2021-08-02 07:13:49 -0400

    Update Screen.h

--- Screen.h
+++ Screen.h
@@ -2,6 +2,7 @@
 #include <SFML/Graphics.hpp>
 #include "BST.h"
 #include "RBT.h"
+#include <time.h>
 class Screen {
 public:
     BST tree;
@@ -12,8 +13,10 @@ public:
     void Draw(sf::RenderWindow& window, sf::Vector2i mousePosition);
     void TopButton(sf::RenderWindow &window);
     void BottomButton(sf::RenderWindow &window);
-};
 
+    clock_t bstStart;
+    clock_t rbtStart;
+};
 
 
 
