3b6885c5029bcab134ce0966c35d892d6ae63937
olivia-813 <73865366+olivia-813@users.noreply.github.com>
2021-08-02 19:53:56 -0400

    Update Screen.h

--- Screen.h
+++ Screen.h
@@ -2,6 +2,7 @@
 #include <SFML/Graphics.hpp>
 #include "BST.h"
 #include "RBT.h"
+#include <cmath>
 #include <time.h>
 class Screen {
 private:
@@ -10,18 +11,24 @@ private:
 public:
     BST tree;
     RBT RBTtree;
+    string fileToLoad;
     clock_t bstStartAgain;
     clock_t rbtStartAgain;
     vector<int> searchThese;
+    bool buttonPress = true;
     Screen();
     void insertBST();
     void insertRBT();
     void doTheStuff();
-    void Draw(sf::RenderWindow& window, sf::Vector2i mousePosition);
+    void Draw(sf::RenderWindow& window, sf::Vector2i mousePosition, int &count);
     void TopButton(sf::RenderWindow &window);
     void BottomButton(sf::RenderWindow &window);
+    void Initial(string file);
+    void Reset(sf::RenderWindow &window);
 
     void setbstTime(double b);
     void setrbtTime(double r);
 
 };
+
+
