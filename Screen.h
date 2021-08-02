75ad3c110350e30df10ab78c1ef3af981c327b7f
olivia-813 <73865366+olivia-813@users.noreply.github.com>
2021-08-02 06:43:58 -0400

    with rbt

--- Screen.h
+++ Screen.h
@@ -1,11 +1,14 @@
 #pragma
 #include <SFML/Graphics.hpp>
 #include "BST.h"
+#include "RBT.h"
 class Screen {
 public:
     BST tree;
+    RBT RBTtree;
     Screen();
     void insertBST();
+    void insertRBT();
     void Draw(sf::RenderWindow& window, sf::Vector2i mousePosition);
     void TopButton(sf::RenderWindow &window);
     void BottomButton(sf::RenderWindow &window);
