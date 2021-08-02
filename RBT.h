0fe6d3209e1d9c2d1686bb09a202ea8944194ea4
Phi Doan <67439394+phidoantn@users.noreply.github.com>
2021-08-01 19:34:44 -0500

    Add files via upload

---
+++ RBT.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+// enum to store color for red black tree
+enum Color { red, black };
+
+// struct for node in RBT
+struct NodeRB {
+
+	int rsid;
+
+	// string of nucleobase
+	string nu;
+
+	// color of node
+	bool color;
+
+	// parent and childs nodes
+	NodeRB* parent;
+	NodeRB* left;
+	NodeRB* right;
+	
+	//constructor for red node
+	NodeRB(int rs, string n) : rsid(rs), nu(n), color(red), parent(nullptr), left(nullptr), right(nullptr) {};
+};
+
+
+// RBT class
+class RBT {
+
+public:
+	NodeRB* root;
+	RBT() :root(nullptr) {};
+	NodeRB* insert(NodeRB* root, int rs, string n);
+	void fixInsert(NodeRB* node);
+
+	NodeRB* getUncle(NodeRB* node);
+
+	void rotateLeft(NodeRB* node);
+	void rotateRight(NodeRB* node);
+
+};
