ce408a8d6dbe170372f19909207842b0fb41dac5
Phi Doan <67439394+phidoantn@users.noreply.github.com>
2021-08-02 07:58:07 -0500

    Update RBT.h

--- RBT.h
+++ RBT.h
@@ -1,7 +1,6 @@
 #pragma once
 #include <string>
 #include <vector>
-
 using namespace std;
 
 // enum to store color for red black tree
@@ -10,21 +9,21 @@ enum Color { red, black };
 // struct for node in RBT
 struct NodeRB {
 
-    int rsid;
+	int rsid;
 
-    // string of nucleobase
-    string nu;
+	// string of nucleobase
+	string nu;
 
-    // color of node
-    bool color;
+	// color of node
+	bool color;
 
-    // parent and childs nodes
-    NodeRB* parent;
-    NodeRB* left;
-    NodeRB* right;
-
-    //constructor for red node
-    NodeRB(int rs, string n) : rsid(rs), nu(n), color(red), parent(nullptr), left(nullptr), right(nullptr) {};
+	// parent and childs nodes
+	NodeRB* parent;
+	NodeRB* left;
+	NodeRB* right;
+	
+	//constructor for red node
+	NodeRB(int rs, string n) : rsid(rs), nu(n), color(red), parent(nullptr), left(nullptr), right(nullptr) {};
 };
 
 
@@ -32,16 +31,18 @@ struct NodeRB {
 class RBT {
 
 public:
-    NodeRB* root;
-    vector<string> RBTtraits;
-    RBT() :root(nullptr) {};
-    NodeRB* insert(NodeRB* root, int rs, string n);
-    void fixInsert(NodeRB* node);
+	NodeRB* root;
+	vector<string> traits;
 
-    NodeRB* getUncle(NodeRB* node);
+	RBT() :root(nullptr) {};
+	NodeRB* insert(NodeRB* root, int rs, string n);
+	void fixInsert(NodeRB* node);
 
-    void rotateLeft(NodeRB* node);
-    void rotateRight(NodeRB* node);
+	NodeRB* getUncle(NodeRB* node);
 
-    void searchRBT(NodeRB* r, vector<int> rsid);
+	void rotateLeft(NodeRB* node);
+	void rotateRight(NodeRB* node);
+
+	void searchRBT(NodeRB* r, vector<int> searchThese);
+
 };
