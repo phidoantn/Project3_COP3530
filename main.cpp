808870c1850bf24d2b26dd06e88d6d931f630005
olivia-813 <73865366+olivia-813@users.noreply.github.com>
2021-08-02 19:54:23 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,42 +5,40 @@
 #include <sstream>
 #include <time.h>
 
+
 int main() {
     Screen send;
+    string input;
+    cout << "Enter file to load: " << endl;
+    cin >> input;
+    input += ".csv";
+    int iter = 0;
+    sf::Vector2i save;
+    send.Initial(input);
 
-    clock_t bstStart;
-    send.insertBST();
-    clock_t bstStop;
-    double elapsedBSTInsert = (double)(bstStart - bstStop) / CLOCKS_PER_SEC;
-    send.setbstTime(elapsedBSTInsert);
+        sf::RenderWindow window(sf::VideoMode(1350, 750), "cop3530 project3");
+        sf::Vector2i mousePositionSave;
 
-    //send.insertRBT(); ///uncomment when rbt is fixed
-    clock_t rstStop;
-    double elapsedRSTInsert = (double)(bstStop - rstStop) / CLOCKS_PER_SEC;
-    send.setrbtTime(elapsedRSTInsert);
+        while (window.isOpen()) {
+            sf::Event event;
 
-    send.doTheStuff();
+            while (window.pollEvent(event)) {
+                if (event.type == sf::Event::Closed)
+                    window.close();
 
-    sf::RenderWindow window(sf::VideoMode(1350, 750), "cop3530 project3");
-    sf::Vector2i mousePositionSave;
-
-    while (window.isOpen()) {
-        sf::Event event;
-
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
-
-            if (event.type == sf::Event::MouseButtonPressed) {
-                sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
-                if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-                    mousePositionSave = mousePosition;                              //save position of click if left click
+                if (event.type == sf::Event::MouseButtonPressed) {
+                    sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
+                    if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+                        save = mousePositionSave;
+                        mousePositionSave = mousePosition;                              //save position of click if left click
+                    }
                 }
-            }
 
-            window.clear(sf::Color(140, 140, 140));
-            send.Draw(window, mousePositionSave);                               //keep drawing board at this position -> until another left click is made
-            window.display();
+                window.clear(sf::Color(140, 140, 140));
+                send.Draw(window, mousePositionSave, iter);                              //keep drawing board at this position -> until another left click is made
+                window.display();
+
         }
     }
+
 }
